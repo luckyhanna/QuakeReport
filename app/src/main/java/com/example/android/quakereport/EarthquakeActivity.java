ccbe26b47a80c196b125e516d69a1baf19704977
Hanna <hanna.botar@appsmart.ro>
2018-06-29 17:16:29 +0300

    final version

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -16,8 +16,11 @@
 package com.example.android.quakereport;
 
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 //import android.os.AsyncTask;
 import android.os.Bundle;
@@ -26,6 +29,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -45,6 +49,7 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
     private EarthquakeAdapter mAdapter;
 
     private TextView emptyView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +61,8 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
 
         emptyView = (TextView) findViewById(R.id.empty_view);
         earthquakeListView.setEmptyView(emptyView);
+
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
 
         mAdapter = new EarthquakeAdapter(this, new ArrayList<Earthquake>());
 
@@ -83,8 +90,32 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
         /*EarthquakeAsyncTask earthquakeAsyncTask = new EarthquakeAsyncTask();
         earthquakeAsyncTask.execute(QueryUtils.USGS_URL);*/
 
-        Log.i(LOG_TAG, "********************* onCreate()");
-        getLoaderManager().initLoader(EARTHQUAKE_LOADER_ID, null, this);
+        /*Log.i(LOG_TAG, "********************* onCreate()");
+        getLoaderManager().initLoader(EARTHQUAKE_LOADER_ID, null, this);*/
+
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+        } else {
+            // Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            progressBar.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            emptyView.setText(R.string.no_internet_connection);
+        }
     }
 
     private void updateUi(List<Earthquake> earthquakes) {
@@ -108,6 +139,8 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
     @Override
     public void onLoadFinished(Loader<List<Earthquake>> loader, List<Earthquake> data) {
         Log.i(LOG_TAG, "********************* onLoadFinished()");
+        progressBar.setVisibility(View.GONE);
+
         updateUi(data);
 
         if (data == null || data.isEmpty()) {
